d048eff8fd92c3ea99311225c6b5eb3043298ca9
leire <asua.leire@gmail.com>
2024-05-24 10:04:38 +0200

    input directory corrected

--- 03.2_denoiseintrons.sh
+++ 03.2_denoiseintrons.sh
@@ -11,8 +11,8 @@
 module load gemoma/1.9
 
 #Introns file from 03.1_ERE
-INT=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/03.1_extraction_RNA_evidence/introns.gff
+INT=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/01_extraction_RNA_evidence/introns.gff
 #Coverage from 03.1_ERE
-CO=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/03.1_extraction_RNA_evidence/coverage.bedgraph
+CO=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/01_extraction_RNA_evidence/coverage.bedgraph
 
 java -Xmx45G -jar /cluster/software/gemoma/GeMoMa-1.9/GeMoMa-1.9.jar CLI DenoiseIntrons i=${INT} coverage_unstranded=${CO}
