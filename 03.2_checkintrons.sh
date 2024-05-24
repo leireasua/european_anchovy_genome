9716ce81942691c723651173e03872b48f7aad64
leire <asua.leire@gmail.com>
2024-05-24 10:04:29 +0200

    input directory corrected

--- 03.2_checkintrons.sh
+++ 03.2_checkintrons.sh
@@ -13,6 +13,6 @@ module load gemoma/1.9
 #Target genome, result obtained from repeatmasker
 GE=/cluster/home/lchueca/TBG_3759_anchovy/04_repeats_annotation/02_repeatmasker/Enen_ragtag_simpl.fasta.masked
 #Introns file from 03.1_ERE
-INT=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/03.1_extraction_RNA_evidence/introns.gff
+INT=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/01_extraction_RNA_evidence/introns.gff
 
 java -jar /cluster/software/gemoma/GeMoMa-1.9/GeMoMa-1.9.jar CLI CheckIntrons t=${GE} i=${INT}
