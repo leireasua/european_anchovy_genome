c3ed9e2ce7af0e04b6af43010492b75f53c92c77
leire <asua.leire@gmail.com>
2024-05-27 11:27:10 +0200

    corrected script

--- 03.5_annotationfinalizer.sh
+++ 03.5_annotationfinalizer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=03.4_annotationfinalizer
+#SBATCH --job-name=03.5_annotationfinalizer
 #SBATCH --error %x-%j.err
 #SBATCH --output %x-%j.out
 
@@ -11,12 +11,12 @@
 module load gemoma/1.9
 
 #Genome assembly after scaffolding and masking
-GE=/cluster/home/lchueca/TBG_3759_anchovy/04_repeats_annotation/02_repeatmasker/Enen_ragtag_simpl.fasta.masked
+GE=/cluster/home/lchueca/TBG_3759_anchovy/04_repeats_annotation/02_repeatmasker/Enen_ragtag_simpl.masked.fasta
 #Annotation obtained from GAF (gemoma annotation filter)
-ANOT=
+ANOT=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/04_GAF/filtered_predictions.gff
 #Introns file
-INT=denoise_introns
+INT=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/02_introns/denoised_introns.gff
 #Coverage from 03.1_ERE
-CO=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/03.1_extraction_RNA_evidence/coverage.bedgraph
+CO=/cluster/home/lchueca/TBG_3759_anchovy/05_annotation/03_gemoma/01_extraction_RNA_evidence/coverage.bedgraph
 
-java -Xmx45G -jar /cluster/software/gemoma/GeMoMa-1.9/GeMoMa-1.9.jar CLI AnnotationFinalizer g=${GE} a=${ANOT} p=Enen_annotation i=${INT} c=${CO}
+java -Xmx45G -jar /cluster/software/gemoma/GeMoMa-1.9/GeMoMa-1.9.jar CLI AnnotationFinalizer g=${GE} a=${ANOT} p=Enen u=YES i=${INT} c=UNSTRANDED coverage_unstranded=${CO}
