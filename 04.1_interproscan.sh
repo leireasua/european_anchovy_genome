cf8ed1b718ac101762bd310949de204d1938b413
leire <asua.leire@gmail.com>
2024-05-31 12:10:50 +0200

    first version

---
+++ 04.1_interproscan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=04.1_interproscan
+#SBATCH --error %x-%j.err
+#SBATCH --output %x-%j.out
+
+#SBATCH --partition=mem
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=64
+
+module load interproscan/5.64-96.0
+
+#Input file in fasta format
+PRO=proteins.fasta
+#Number of threads
+CPU=64
+
+interproscan.sh -i ${IN} -f tsv -iprlookup -b Enen -pa -goterms -exclappl SignalP_GRAM_NEGATIVE,SignalP_GRAM_POSITIVE -dp -cpu ${CPU}
+
