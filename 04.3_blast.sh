fda5b3e01aede5cb8f6d081c45e175f584ac19ce
leire <asua.leire@gmail.com>
2024-06-11 10:10:15 +0200

    database changed

--- 04.3_blast.sh
+++ 04.3_blast.sh
@@ -5,20 +5,20 @@
 #SBATCH --output %x-%j.out
 
 #SBATCH --partition=mem
-#SBATCH --mem=10G
+#SBATCH --mem=30G
 #SBATCH --cpus-per-task=64
 
 module load ncbi-blast/2.15.0
 
 #Input file in fasta format
-PRO=proteins.fasta
+PRO=proteins02.fasta
 #Number of threads
 CPU=64
 
-# blast vs swissprot for putative names:
+#uniprot_sprot_2024-05.fasta was downloaded using wget from uniprot
 
-makeblastdb -in uniprot_sprot_2023-05.fasta -out uniprot_sprot_2023-05.fasta -parse_seqids -dbtype prot &&
+#blast vs swissprot for putative names:
 
-blastp -num_threads ${CPU} -query ${PRO} -db uniprot_sprot_2023-05.fasta -evalue 1e-6 -max_hsps 1 -max_target_seqs 1 -outfmt 6 -out Enen_proteins.fasta.blastp
+makeblastdb -in uniprot_sprot_2024-05.fasta -out uniprot_sprot_2024-05.fasta -parse_seqids -dbtype prot &&
 
-
+blastp -num_threads ${CPU} -query ${PRO} -db uniprot_sprot_2024-05.fasta -evalue 1e-6 -max_hsps 1 -max_target_seqs 1 -outfmt 6 -out Enen_proteins.fasta.blastp
